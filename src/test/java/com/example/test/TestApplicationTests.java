7d186fa885b9b45f7ebc4fd2400615caf73c218e
ewycheung <cheung_eddie@rogers.com>
2021-03-16 19:59:21 -0400

    update unit test

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
